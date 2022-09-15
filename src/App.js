4574c06ac1bacc2106692d67bb4cf7d6dbcdbd03
ckrb63 <cksr1@naver.com>
2022-09-15 13:11:34 +0900

    Initial project setting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { MAIN_PATH, ROOT_PATH } from "./common/constants/path.const";
+import LoginPage from "./pages/Login/LoginPage";
+import MainPage from "./pages/Main/MainPage";
+
 function App() {
   return (
-    <div>
-      <button>Hello World!</button>
-      <div>나는야 고구마</div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path={ROOT_PATH} element={<LoginPage/>} />
+        <Route path={MAIN_PATH} element={<MainPage/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
