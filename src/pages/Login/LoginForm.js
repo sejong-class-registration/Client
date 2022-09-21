9d452a41bedd308b63528aa93783af2a3c6f67be
gabean13 <gazette9@naver.com>
2022-09-21 21:11:00 +0900

    [ADD] 로그인 페이지 구현

---
+++ src/pages/Login/LoginForm.js
@@ -0,0 +1,106 @@
+import { useState } from "react";
+import { useRef } from "react";
+import { Link } from "react-router-dom";
+import "./LoginForm.scss";
+
+const LoginForm = () => {
+  const EnteredId = useRef();
+  const EnteredPw = useRef();
+  const [EnteredIdisValid, setEnteredIdisValid] = useState(true);
+  const [EnteredPwisValid, setEnteredPwisValid] = useState(true);
+  const [pwInputisTouched, setpwInputisTouched] = useState(false);
+  const [IdInputisTouched, setIdInputisTouched] = useState(false);
+
+  const idInputHandler = () => {
+    setIdInputisTouched(true);
+    const id = EnteredId.current.value;
+
+    if (id.trim() === "" || isNaN(id)) {
+      setEnteredIdisValid(false);
+    } else {
+      setEnteredIdisValid(true);
+    }
+  };
+  const pwInputHandler = () => {
+    setpwInputisTouched(true);
+    const pw = EnteredPw.current.value;
+
+    if (pw.trim() === "") {
+      setEnteredPwisValid(false);
+    } else {
+      setEnteredPwisValid(true);
+    }
+  };
+
+  const LoginSubmitHandler = (event) => {
+    event.preventDefault();
+
+    if (
+      (!EnteredIdisValid &&
+        IdInputisTouched &&
+        !EnteredPwisValid &&
+        pwInputisTouched) ||
+      !IdInputisTouched || !pwInputisTouched
+    ) {
+      setEnteredIdisValid(false);
+      setEnteredPwisValid(false);
+      return;
+    } else if ((!EnteredIdisValid && IdInputisTouched) || !IdInputisTouched) {
+      setEnteredIdisValid(false);
+      return;
+    } else if ((!EnteredPwisValid && pwInputisTouched) || !pwInputisTouched) {
+      setEnteredPwisValid(false);
+      return;
+    }
+
+    const EnteredUserInfo = {
+      id: EnteredId.current.value,
+      pw: EnteredPw.current.value,
+    };
+    console.log(EnteredUserInfo);
+  };
+
+  const idInputClassName = EnteredIdisValid ? "" : "login-invalid-input";
+  const pwInputClassName = EnteredPwisValid ? "" : "login-invalid-input";
+
+  return (
+    <form className="login-formbox" onSubmit={LoginSubmitHandler}>
+      <p>학번/아이디</p>
+      <input
+        className={idInputClassName}
+        type="text"
+        id="id"
+        onBlur={idInputHandler}
+        ref={EnteredId}
+      />
+      {!EnteredIdisValid && (
+        <p className="login-invalid">아이디(학번)를 입력해주세요</p>
+      )}
+      <p>비밀번호</p>
+      <input
+        className={pwInputClassName}
+        type="password"
+        id="pw"
+        onBlur={pwInputHandler}
+        ref={EnteredPw}
+      />
+      {!EnteredPwisValid && (
+        <p className="login-invalid">비밀번호를 입력해주세요</p>
+      )}
+      <div className="login-formbox-help">
+        <span>
+          <Link to="/findid">아이디 찾기</Link>
+        </span>
+        <span>
+          <Link to="/findpw">비밀번호 찾기</Link>
+        </span>
+        <span>
+          <Link to="/signup">회원가입</Link>
+        </span>
+      </div>
+      <button>로그인</button>
+    </form>
+  );
+};
+
+export default LoginForm;
--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import LoginForm from "./LoginForm";
+import "./LoginPage.scss";
 
 function LoginPage() {
-  return <div></div>
+  return (
+    <div className="login-background">
+      <img
+        src="https://portal.sejong.ac.kr/content/02comm/css/images/login/login_logo.png"
+        alt="logo"
+        className="login-logo"
+      />
+      <div className="login-text">
+        <p className="login-text-title">수강신청 도우미</p>
+        <p className="login-text-exp">
+          서비스 이용을 끝낸 후에는 개인정보보호를 위하여 꼭{" "}
+          <span>로그아웃</span>을 해주시기 바랍니다.
+        </p>
+      </div>
+      <LoginForm />
+    </div>
+  );
 }
 
 export default LoginPage;
