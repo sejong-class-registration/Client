d21241b6490cbefeafd7ae6f7b50808ac5d1e264
dlrlseong <leekisung0212@gmail.com>
2022-09-22 17:26:50 +0900

    [ADD] add modal

--- src/pages/Main/Modal.js
+++ src/pages/Main/Modal.js
@@ -7,9 +7,54 @@ const Backdrop = (props) => {
 };
 
 const ModalOverlay = (props) => {
+  // const majorList = [
+  //   "AI연계융합전공[3317 학부]연계전공",
+  //   "AI연계융합전공 소셜미디어매니지먼트소프트웨어[3328 학부]연계전공",
+  //   "AI연계융합전공 스마트투어리즘매니지멘트소프트웨어[3331 학부]연계전공",
+  //   "AI연계융합전공 시스템생명공학[3324 학부]연계전공",
+  //   "AI연계융합전공 에듀테크콘텐츠애널리틱스[3326 학부]연계전공",
+  // ];
+  // const returnMajor = (props) => {
+  //   return props.map((major, index) => {
+  //     <option key={index} value={major}>
+  //       {major}
+  //     </option>;
+  //   });
+  // };
   return (
     <div className="modal">
-      <button onClick={props.close}>close</button>
+      <div className="modal-closeButton">
+        <button className="modal-closeButton-button" onClick={props.close}>x</button>
+      </div>
+      <div className="modal-contents">
+        <div className="modal-contents-first">
+          <label className="modal-contents-first-label">학과전공</label>
+          <select className="modal-contents-first-select">
+            <option value="AI연계융합전공[3317 학부]연계전공">
+              AI연계융합전공[3317 학부]연계전공
+            </option>
+            <option value="test">test</option>
+          </select>
+        </div>
+        <div className="modal-contents-second">
+          <label className="modal-contents-second-label">교과목명</label>
+          <input className="modal-contents-second-input"></input>
+        </div>
+        <div className="modal-contents-third">
+          <div className="modal-contents-third-prof">
+            <label className="modal-contents-third-prof-label">교수명</label>
+            <input className="modal-contents-third-prof-input"></input>
+          </div>
+          <div className="modal-contents-third-type">
+            <label className="modal-contents-third-type-label">이수구분</label>
+            <select className="modal-contents-third-type-select">
+              <option value="test">test</option>
+              <option value="test">test</option>
+            </select>
+          </div>
+          <button className="modal-contents-third-button">조회</button>
+        </div>
+      </div>
     </div>
   );
 };
@@ -22,11 +67,7 @@ const Modal = (props) => {
         document.getElementById("backdrop-root")
       )}
       {ReactDOM.createPortal(
-        <ModalOverlay
-          title="title test"
-          message="Hello World!"
-          close={props.close}
-        />,
+        <ModalOverlay close={props.close} />,
         document.getElementById("overlay-root")
       )}
     </>
