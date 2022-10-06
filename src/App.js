ac99ac3e65d3f2632c530833bcf57acdcc21c6de
gabean13 <gazette9@naver.com>
2022-10-06 17:12:11 +0900

    [ADD] 비밀번호 찾기 페이지

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { LECTURE_PATH, MAIN_PATH, ROOT_PATH, SIGNUP_PATH } from "./common/constants/path.const";
+import { FINDPW_PATH, LECTURE_PATH, MAIN_PATH, ROOT_PATH, SIGNUP_PATH } from "./common/constants/path.const";
 import LoginPage from "./pages/Login/LoginPage";
 import MainPage from "./pages/Main/MainPage";
 import RecommendedLecturePage from "./pages/RecommendedLecture/RecommendedLecturePage";
-import './App.scss';
 import Signup from "./pages/Signup/Signup";
+import FindPw from "./pages/FindPw/FindPw";
+import './App.scss';
 
 function App() {
   
@@ -16,6 +17,7 @@ function App() {
         <Route path={MAIN_PATH} element={<MainPage/>}/>
         <Route path={LECTURE_PATH} element = {<RecommendedLecturePage/>}/>
         <Route path={SIGNUP_PATH} element = {<Signup/>}/>
+        <Route path={FINDPW_PATH} element = {<FindPw/>}/>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/FindPw/FindPw.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./FindPw.scss";
+
+const FindPw = () => {
+  return (
+    <div className="findpw-background">
+      <img
+        src="https://portal.sejong.ac.kr/content/02comm/css/images/login/login_logo.png"
+        alt="logo"
+        className="login-logo"
+      />
+      <div className="findpw-text">
+        <p>수강신청 도우미</p>
+        <p>비밀번호 찾기</p>
+      </div>
+      <div className="findpw-text-exp">
+        <p>학생 인증을 위해</p>
+        <span>세종대학교 학사정보시스템</span> 학번과 비밀번호를
+        <p>입력해주시기 바랍니다</p>
+      </div>
+      <div>
+        
+      </div>
+      <div className="login-copyright">
+        <p>copyright ⓒ 2022 열일곱스물하나</p>
+        <p>이기성 김찬규 최가빈 김윤희</p>
+      </div>
+    </div>
+  );
+};
+
+export default FindPw;
