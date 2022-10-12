2cad344d3ac2b9b4e8fa37f9632a08727e7c84d8
gabean13 <gazette9@naver.com>
2022-10-12 20:14:02 +0900

    [ADD] 로그인form 사용자입력피드백제공 방법 수정

--- src/pages/Login/LoginForm.js
+++ src/pages/Login/LoginForm.js
@@ -1,70 +1,83 @@
+import { useEffect } from "react";
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./LoginForm.scss";
 
 const LoginForm = () => {
-  const [EnteredInput, setEnteredInput] = useState({id: '', pw: ''});
-  const [EnteredInputIsValid, setEnteredInputIsValid] = useState({id: true, pw: true});
-  const [inputIsTouched, setInputIsTouched] = useState({id: false, pw: false});
+  const [EnteredInput, setEnteredInput] = useState({ id: "", pw: "" });
+  const [EnteredInputIsValid, setEnteredInputIsValid] = useState({
+    id: true,
+    pw: true,
+  });
+  const [inputIsTouched, setInputIsTouched] = useState({
+    id: false,
+    pw: false,
+  });
 
   const inputChangeHandler = (event) => {
-    setEnteredInput((prev)=>{
-      return (
-        {...prev, [event.target.id] : event.target.value}
-      )
-    })
-  }
+    setEnteredInput((prev) => {
+      return { ...prev, [event.target.id]: event.target.value };
+    });
+  };
 
   const inputBlurHandler = (e) => {
+    console.log('inputBlurHandler');
     const selectedId = e.target.id;
-    setInputIsTouched((prev)=>{
-      return {...prev, [selectedId]: true};
-    })
+    setInputIsTouched((prev) => {
+      return { ...prev, [selectedId]: true };
+    });
 
-    if(selectedId === 'id'){
+    if (selectedId === "id") {
       if (EnteredInput.id.trim() === "" || isNaN(EnteredInput.id)) {
-        setEnteredInputIsValid((prev)=>{
-          return {...prev, id : false}
+        setEnteredInputIsValid((prev) => {
+          return { ...prev, id: false };
         });
       } else {
-        setEnteredInputIsValid((prev)=>{
-          return {...prev, id : true}
+        setEnteredInputIsValid((prev) => {
+          return { ...prev, id: true };
         });
       }
-    }else{
+    } else {
       if (EnteredInput.pw.trim() === "") {
-        setEnteredInputIsValid((prev)=>{
-          return {...prev, pw : false}
+        setEnteredInputIsValid((prev) => {
+          return { ...prev, pw: false };
         });
       } else {
-        setEnteredInputIsValid((prev)=>{
-          return {...prev, pw : true}
+        setEnteredInputIsValid((prev) => {
+          return { ...prev, pw: true };
         });
       }
     }
-
   };
 
   const LoginSubmitHandler = (event) => {
-    event.preventDefault();
+    console.log('LoginSubmit');
 
+    event.preventDefault();
     if (
       (!EnteredInputIsValid.id &&
         inputIsTouched.id &&
         !EnteredInputIsValid.pw &&
         inputIsTouched.pw) ||
-      !inputIsTouched.id || !inputIsTouched.pw
+      !inputIsTouched.id ||
+      !inputIsTouched.pw
     ) {
-      setEnteredInputIsValid({id: false, pw: false});
+      setEnteredInputIsValid({ id: false, pw: false });
       return;
-    } else if ((!EnteredInputIsValid.id && inputIsTouched.id) || !inputIsTouched.id) {
-      setEnteredInputIsValid((prev)=>{
-        return {...prev, id : false}
+    } else if (
+      (!EnteredInputIsValid.id && inputIsTouched.id) ||
+      !inputIsTouched.id
+    ) {
+      setEnteredInputIsValid((prev) => {
+        return { ...prev, id: false };
       });
       return;
-    } else if ((!EnteredInputIsValid.pw && inputIsTouched.pw) || !inputIsTouched.pw) {
-      setEnteredInputIsValid((prev)=>{
-        return {...prev, pw : false}
+    } else if (
+      (!EnteredInputIsValid.pw && inputIsTouched.pw) ||
+      !inputIsTouched.pw
+    ) {
+      setEnteredInputIsValid((prev) => {
+        return { ...prev, pw: false };
       });
       return;
     }
@@ -73,42 +86,47 @@ const LoginForm = () => {
   };
 
   const loginFetchHandler = () => {
-
-    fetch('http://127.0.0.1:3000/users/signin', {
-      method: 'POST',
+    console.log('inputBlurHandler');
+    fetch("http://127.0.0.1:3000/users/signin", {
+      method: "POST",
       body: JSON.stringify({
         studentId: EnteredInput.id,
         password: EnteredInput.pw,
-      })
-    })
-    .then((response) => {
-      if(response.message === 'Success'){
-        window.localStorage.setItem('token', response.token);
+      }),
+    }).then((response) => {
+      if (response.message === "Success") {
+        window.localStorage.setItem("token", response.token);
         goToMain();
-      }else{
-        alert('아이디 또는 비밀번호가 일치하지않습니다.');
-        setEnteredInput({id: '', pw: ''});
+      } else {
+        alert("아이디 또는 비밀번호가 일치하지않습니다.");
+        setEnteredInput({ id: "", pw: "" });
       }
-    })
-  }
+    });
+  };
 
   const goToMain = () => {
-    window.location.replace('/main');
+    window.location.replace("/main");
+  };
+
+  const onCheckEnter = (event) =>{
+    if(event.key === 'Enter'){
+      inputBlurHandler(event)
+    }
   }
 
   const idInputClassName = EnteredInputIsValid.id ? "" : "login-invalid-input";
   const pwInputClassName = EnteredInputIsValid.pw ? "" : "login-invalid-input";
 
   return (
-    <form onSubmit={LoginSubmitHandler} className = 'login-formbox'>
+    <form onKeyPress={onCheckEnter} onSubmit={LoginSubmitHandler} className="login-formbox">
       <p>학번/아이디</p>
       <input
         className={idInputClassName}
         type="text"
         id="id"
         onBlur={inputBlurHandler}
-        onChange = {inputChangeHandler}
-        value = {EnteredInput.id}
+        onChange={inputChangeHandler}
+        value={EnteredInput.id}
       />
       {!EnteredInputIsValid.id && (
         <p className="login-invalid">아이디(학번)를 입력해주세요</p>
@@ -119,8 +137,8 @@ const LoginForm = () => {
         type="password"
         id="pw"
         onBlur={inputBlurHandler}
-        onChange = {inputChangeHandler}
-        value = {EnteredInput.pw}
+        onChange={inputChangeHandler}
+        value={EnteredInput.pw}
       />
       {!EnteredInputIsValid.pw && (
         <p className="login-invalid">비밀번호를 입력해주세요</p>
