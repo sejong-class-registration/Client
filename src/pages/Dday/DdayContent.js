8476d74b5beeee495d25d27c61a17b42568b2f01
dlrlseong <leekisung0212@gmail.com>
2022-11-17 15:10:02 +0900

    [ADD] add dday page and frame content

---
+++ src/pages/Dday/DdayContent.js
@@ -0,0 +1,15 @@
+import "./DdayContent.scss";
+const DdayContent = () => {
+  return (
+    <div className="wrapper">
+      <div className="ddayContent">
+        <div className="ddayContent-box"></div>
+        <div className="ddayContent-box"></div>
+        <div className="ddayContent-box"></div>
+      </div>
+      <div className="ddayImage"></div>
+    </div>
+  );
+};
+
+export default DdayContent;
