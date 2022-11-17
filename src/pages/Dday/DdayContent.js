3c480aaf64aacdf4cc94576c3eb417ecb08b77c0
dlrlseong <leekisung0212@gmail.com>
2022-11-17 15:19:40 +0900

    [ADD] dday contents 추가중

--- src/pages/Dday/DdayContent.js
+++ src/pages/Dday/DdayContent.js
@@ -3,7 +3,13 @@ const DdayContent = () => {
   return (
     <div className="wrapper">
       <div className="ddayContent">
-        <div className="ddayContent-box"></div>
+        <div className="ddayContent-box">
+          <div className="ddayContent-box-name">컴퓨터구조</div>
+          <div className="ddayContent-box-others">
+            <div className="ddayContent-box-others-lectureId"></div>
+            <div className="ddayContent-box-others-distrib"></div>
+          </div>
+        </div>
         <div className="ddayContent-box"></div>
         <div className="ddayContent-box"></div>
       </div>
